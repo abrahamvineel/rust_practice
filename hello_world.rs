7cc18bc6287b105baedcd479edf8a3f0c29100aa
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-02-29 21:00:07 -0500

    mutable variables in rust

--- hello_world.rs
+++ hello_world.rs
@@ -1,8 +1,10 @@
 fn main() {
-    let a_number;
+    let mut a_number;
     let a_word = "Ten";
 
     a_number = 10;
+    println!("The number is {}.", a_number);
+    a_number = 15;
     println!("The number is {}.", a_number);
     println!("The word is {}.", a_word);
 }
