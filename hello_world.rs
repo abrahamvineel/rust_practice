db8c38695a9e6d78cb4fa9a7d6971db7215dbae5
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-03-05 20:54:20 -0500

    Update hello_world.rs
    int and float div

--- hello_world.rs
+++ hello_world.rs
@@ -19,4 +19,6 @@ fn main() {
     println!("The number is {}.", number);
 
     println!("1 + 2 = {} and 8 - 5 = {} and 15 * 3 = {}", 1u32 + 2, 8i32 - 5, 15 * 3);
+
+    println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
 }
