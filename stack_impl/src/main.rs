39defaac9f1a983d98217e9cdcb5b3db12920773
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-03-19 21:24:48 -0400

    postfix

--- stack_impl/src/main.rs
+++ stack_impl/src/main.rs
@@ -1,15 +1,15 @@
-fn new_stack(maxsize: usize) -> Vec<u32> {
-    let vec: Vec<u32> = Vec::with_capacity(maxsize);
+fn new_stack(maxsize: usize) -> Vec<String> {
+    let vec: Vec<String> = Vec::with_capacity(maxsize);
     vec
 }
 
-fn pop(stack: &mut Vec<u32>) -> Option<u32> {
-    let popped_val: Option<u32> = stack.pop();
+fn pop(stack: &mut Vec<String>) -> Option<String> {
+    let popped_val: Option<String> = stack.pop();
     println!("The popped value is {:?} ", popped_val);
     popped_val
 }
 
-fn push(stack: &mut Vec<u32>, item: u32, maxsize: usize) {
+fn push(stack: &mut Vec<String>, item: String, maxsize: usize) {
     if stack.len() == maxsize {
         println!("Stack size full");
     } else {
@@ -18,11 +18,11 @@ fn push(stack: &mut Vec<u32>, item: u32, maxsize: usize) {
     }
 }
 
-fn stack_size(stack: &Vec<u32>) -> usize {
+fn stack_size(stack: &Vec<String>) -> usize {
     stack.len()
 }
 
-fn input() -> u32 {
+fn input() -> String {
     let mut n = String::new();
     std::io::stdin()
         .read_line(&mut n)
@@ -32,22 +32,11 @@ fn input() -> u32 {
     n
 }
 
-fn main() {
-    let size = input();
+fn individual_symbols(input_expr: String) -> Vec<String> {
+    // let mut tokenized_input:Vec<String> = Vec::new();
+}
 
-    let mut stack = new_stack(size as usize);
-    loop {
-        let option = input();
-        match option {
-            1 => {
-                let item = input();
-                push(&mut stack, item, size as usize);
-            }
-            2 => println!("popped val {:?}", pop(&mut stack)),
-            3 => println!("The elements are {:?}", stack),
-            4 => println!("The size of stack {}", stack_size(&stack)),
-            5 => break,
-            _ => println!("Invalid option")
-        }
-    }
+fn main() {
+    let input_expr = String::from("(33+45/3*(2+9)-50)");
+    println!("The original expr is {:?}", input_expr);
 }
