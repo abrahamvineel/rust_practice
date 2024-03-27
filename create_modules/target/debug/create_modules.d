f3fa18b2ad4fab1e89425c93456358a32c7c7453
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-03-26 20:01:08 -0400

    modules and crate

---
+++ create_modules/target/debug/create_modules.d
@@ -0,0 +1,1 @@
+C:\Pro\rust_practice\create_modules\target\debug\create_modules.exe: C:\Pro\rust_practice\create_modules\src\main.rs C:\Pro\rust_practice\create_modules\src\sample.rs
---
+++ create_modules/target/debug/deps/create_modules.d
@@ -0,0 +1,6 @@
+C:\Pro\rust_practice\create_modules\target\debug\deps\create_modules.exe: src\main.rs src\sample.rs
+
+C:\Pro\rust_practice\create_modules\target\debug\deps\create_modules.d: src\main.rs src\sample.rs
+
+src\main.rs:
+src\sample.rs:
