b9e649e7bcbef23773b5fe7ab83eb84d823f2526
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-03-28 20:29:44 -0400

    Update hello_world.rs
    move struct outside main

--- hello_world.rs
+++ hello_world.rs
@@ -27,12 +27,15 @@ fn main() {
 
     let tuple_e = ('E', 5i32, true);
     println!("Is '{}' the {}th letter of the alphabet? {}", tuple_e.0, tuple_e.1, tuple_e.2);
-
     
-    struct Student { name: String, level: u8, remote: bool }
     let user_1 = Student { name: String::from("hello"), remote: true, level: 2 };
     println!("{}, level {}. Remote: {}. ", user_1.name, user_1.level, user_1.remote);
 
     let user_2 = Student { name: String::from("world"), remote: false, level: 1};
-    
+}
+
+struct Student {
+    name: String,
+    level: u8,
+    remote: bool
 }
