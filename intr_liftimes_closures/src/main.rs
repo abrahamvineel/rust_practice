9fc9e64f9e1e80ceb1d7252ce1eb056728bc3f4c
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-05-24 17:04:26 -0400

    Update main.rs

--- intr_liftimes_closures/src/main.rs
+++ intr_liftimes_closures/src/main.rs
@@ -3,27 +3,27 @@ fn f1(i: &i32) -> &i32 {
     &i
 }
 
+// fn main() {
+//     let i = 10;
+//     let val = f1(&i);
+//     println!("{}", val);
+// }
+
+
 fn main() {
-    let i = 10;
-    let val = f1(&i);
-    println!("{}", val);
+    let s1 = "hello";
+    let a;
+
+    {
+        let s2 = String::from("world");
+        a = f1(s1, s2.as_str());
+    }
+    println!("{}", a);
 }
 
-
-// fn main() {
-//     let s1 = "hello";
-//     let a;
-//
-//     {
-//         let s2 = String::from("world");
-//         a = f1(s1, s2.as_str());
-//     }
-//     println!("{}", a);
-// }
-//
-// fn f1<'a, 'b>(str1: &'a str, str2: &'b str) -> &'a str {
-//     str1
-// }
+fn f1<'a, 'b>(str1: &'a str, str2: &'b str) -> &'a str {
+    str1
+}
 
 // fn main() {
 //     let i1 = 5;
